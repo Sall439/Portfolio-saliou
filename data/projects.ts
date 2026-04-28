649f2a4d016204eb1f97bec7645280e23c06586b
Saliou Sall <salljunior439@gmail.com>
2026-04-28 16:09:05 +0100

    corrr

--- data/projects.ts
+++ data/projects.ts
@@ -4,8 +4,8 @@ export interface Project {
   description: string;
   tech: string[];
   link: string;
-  src?: string;    // Optionnel car Setal App n'en a pas encore
-  color?: string;  // Optionnel car Junior Clothes n'en a pas
+  src?: string;   
+  color?: string;  
 }
 
 // 2. On applique cette interface au tableau (et on enlève "as const")
@@ -15,15 +15,16 @@ export const projects: Project[] = [
     description: 'E-commerce platform for clothing with modern UI, product catalog, shopping cart, and seamless checkout experience.',
     tech: ['Html', 'Css', 'JavaScript'],
     link: 'https://juniorclothes-ab1j.vercel.app/',
-    src: '/assets/jclothes.png'
-    // Pas besoin de color ici
+    src: '/assets/jclothes.png',
+     color: ""
   },
   {
     title: 'Etamp Sarl',
     description: 'Corporate website for a Senegalese company based in Diamniadio. Features company presentation, services, and contact information.',
     tech: ['Html', 'Css', 'JavaScript'],
     link: 'https://www.etampsarl.com/',
-    src: '/assets/etamp.png'
+    src: '/assets/etamp.png',
+     color: ""
   },
   {
     title: 'Setal App',
