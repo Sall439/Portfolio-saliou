60fa08bdc05ec04cef32e69ee1b7bcee7da0f1f2
Saliou Sall <salljunior439@gmail.com>
2026-04-28 16:40:53 +0100

    ddjdjd

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Skills from '@/components/Skills';
 import Process from '@/components/Process';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
+import { ToastContainer } from 'react-toastify';
 
 export default function Home() {
   return (
@@ -25,6 +26,7 @@ export default function Home() {
       <Process />
       <Contact />
       <Footer />
+      <ToastContainer position='top-right' autoClose={3000} />
     </div>
   );
 }
