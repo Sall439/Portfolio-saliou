7d0740fcd36cd5de8d4da86257af2f594515fa92
Saliou Sall <salljunior439@gmail.com>
2026-05-21 13:49:14 +0100

    maj des projects

--- data/projects.ts
+++ data/projects.ts
@@ -11,24 +11,24 @@ export interface Project {
 // 2. On applique cette interface au tableau (et on enlève "as const")
 export const projects: Project[] = [
   {
-    title: 'Junior Clothes',
-    description: 'E-commerce platform for clothing with modern UI, product catalog, shopping cart, and seamless checkout experience.',
-    tech: ['Html', 'Css', 'JavaScript'],
-    link: 'https://juniorclothes-ab1j.vercel.app/',
-    src: '/assets/jclothes.png',
-     color: ""
+    title: 'Eventify',
+    description: 'Une plateforme complète de gestion d’événements et de prise de rendez-vous, intégrant le suivi des disponibilités en temps réel et l’automatisation des plannings.',
+    tech: ['ReactJs', 'TailwindCss', 'Laravel'],
+    link: 'https://eventify-front-4.vercel.app/',
+    src: '/assets/eventify.png',
+    color: ""
   },
   {
     title: 'Etamp Sarl',
-    description: 'Corporate website for a Senegalese company based in Diamniadio. Features company presentation, services, and contact information.',
+    description: 'Site vitrine d’entreprise pour une société sénégalaise basée à Diamniadio, comprenant la présentation des services, l’histoire de la compagnie et un espace de contact.',
     tech: ['Html', 'Css', 'JavaScript'],
     link: 'https://www.etampsarl.com/',
     src: '/assets/etamp.png',
-     color: ""
+    color: ""
   },
   {
     title: 'Setal App',
-    description: 'An innovative application currently in development. Stay tuned for updates on this exciting project.',
+    description: 'Une application mobile innovante axée sur la gestion de la propreté, actuellement en cours de développement. Restez connectés pour suivre les prochaines mises à jour.',
     tech: ['React Native', 'Node.js', 'MongoDB', 'Expo'],
     color: 'from-blue-500 to-indigo-500',
     link: '#',
